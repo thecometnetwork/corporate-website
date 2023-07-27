996db238d99f72cd22bd7d8e9891edf3c9b9b2db
gabrieletesser <gabriele.tesser@seznam.cz>
2023-07-27 13:14:53 +0200

    fix: added missing package

--- app/admin/(admin-dashboard)/(lists)/pages/new/page.jsx
+++ app/admin/(admin-dashboard)/(lists)/pages/new/page.jsx
@@ -53,21 +53,6 @@ export default function page() {
                   <CKEditor
                     editor={ClassicEditor}
                     data="<p>Hello from CKEditor 5!</p>"
-                    onReady={(editor) => {
-                      // You can store the "editor" and use when it is needed.
-                      console.log("Editor is ready to use!", editor);
-                    }}
-                    onChange={(event, editor) => {
-                      const data = editor.getData();
-                      console.log({event, editor, data});
-                      setContent(data);
-                    }}
-                    onBlur={(event, editor) => {
-                      console.log("Blur.", editor);
-                    }}
-                    onFocus={(event, editor) => {
-                      console.log("Focus.", editor);
-                    }}
                   />
                 </>
               );
